663da859a2755da530933cf2a65e8418082de911
sangaryousmane <sangaryousmane@gmail.com>
2024-03-21 23:39:14 +0000

    Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,26 +1,17 @@
 -- Compute Average Weighted score for user
-DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    user_id INT
+)
 BEGIN
-    DECLARE total_score DECIMAL(10, 2);
-    DECLARE total_weight DECIMAL(10, 2);
-    DECLARE avg_weighted_score DECIMAL(10, 2);
-    
-    SELECT SUM(score * weight) INTO total_score
-    FROM scores
-    WHERE user_id = user_id;
-    
-    SELECT SUM(weight) INTO total_weight
-    FROM scores
-    WHERE  user_id = user_id;
-    
-    SET avg_weighted_score = total_score / total_weight;
-    
-    INSERT INTO average_weighted_scores (user_id, average_score) 
-    VALUES (user_id, avg_weighted_score)
-    ON DUPLICATE KEY UPDATE average_score = avg_weighted_score;
-END;
-//
-
-DELIMITER ;
+    DECLARE w_avg_score FLOAT;
+    SET weighted_avg_score = (SELECT SUM(score * weight) / SUM(weight)
+                        FROM users AS u
+                        JOIN corrections as c ON u.id=c.user_id
+                        JOIN projects AS p ON c.project_id=p.id
+                        WHERE u.id=user_id);
+    UPDATE users SET average_score = weighted_avg_score WHERE id=user_id;
+END
+$$
