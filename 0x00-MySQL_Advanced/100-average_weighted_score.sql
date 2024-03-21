a51dfb6900b3a6c94087d58d163a15b5d8e09692
sangaryousmane <sangaryousmane@gmail.com>
2024-03-21 23:36:28 +0000

    Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,3 +1,4 @@
+-- Compute Average Weighted score for user
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
@@ -6,20 +7,16 @@ BEGIN
     DECLARE total_weight DECIMAL(10, 2);
     DECLARE avg_weighted_score DECIMAL(10, 2);
     
-    -- Calculate the total weighted score
     SELECT SUM(score * weight) INTO total_score
     FROM scores
     WHERE user_id = user_id;
     
-    -- Calculate the total weight
     SELECT SUM(weight) INTO total_weight
     FROM scores
     WHERE  user_id = user_id;
     
-    -- Compute the average weighted score
     SET avg_weighted_score = total_score / total_weight;
     
-    -- Store the average weighted score in a separate table or update an existing record
     INSERT INTO average_weighted_scores (user_id, average_score) 
     VALUES (user_id, avg_weighted_score)
     ON DUPLICATE KEY UPDATE average_score = avg_weighted_score;
@@ -27,4 +24,3 @@ END;
 //
 
 DELIMITER ;
-
