d0f77ff591b46ccf95fa26bbc41992c25c3dc8e0
sangaryousmane <sangaryousmane@gmail.com>
2024-03-21 15:32:35 +0000

    In and not out users with enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- CREATE A TABLE name users with enumeration
+
+
+CREATE TABLE IF NOT EXISTS users
+(
+id INT PRIMARY KEY AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'IN') NOT NULL DEFAULT 'US'
+);
