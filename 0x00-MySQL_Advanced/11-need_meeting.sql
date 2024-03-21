0967bb21a5a9a41de9b146a1c6f1647a2c252feb
sangaryousmane <sangaryousmane@gmail.com>
2024-03-21 19:09:00 +0000

    No table for a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS 
 SELECT name FROM students 
-WHERE score < 80 AND (last_meeting IS NULL OR last_meeting ADDDATE(CURDATE(), INTERVAL -1 MONTH));
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
