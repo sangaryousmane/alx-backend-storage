8e21bca3ca3fc6ea89070b2b8cb312ecd7b68f3a
sangaryousmane <sangaryousmane@gmail.com>
2024-03-21 18:38:03 +0000

    No table for a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,5 +3,5 @@
 -- under 80 (strict) and no last_meeting or more than 1 month.
 
 CREATE VIEW need_meeting AS 
-SELECT *FROM students 
+SELECT name FROM students 
 WHERE score < 80 AND (last_meeting IS NULL OR last_meeting DATE_SUB(NOW(), INTERVAL 1 MONTH));
