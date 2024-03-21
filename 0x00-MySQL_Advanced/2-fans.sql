418e77c8e38084b352a3e1b8fac66748cf3bb986
sangaryousmane <sangaryousmane@gmail.com>
2024-03-21 17:20:23 +0000

    Best band ever!

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- Write a SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+
+
+SELECT origin, COUNT(*) as nb_fans
+FROM  metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
