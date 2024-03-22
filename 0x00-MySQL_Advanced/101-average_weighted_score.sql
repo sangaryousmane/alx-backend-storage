8859c5a2c0546aca3f80647addd25281c4968611
sangaryousmane <sangaryousmane@gmail.com>
2024-03-22 00:03:17 +0000

    Average weighted score for all!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,13 +5,13 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     UPDATE users as u,
-    (SELECT u.id,  SUM(score * weight) / SUM(weight) AS wa
+        (SELECT u.id,  SUM(score * weight) / SUM(weight) AS wa
          FROM users AS u
          JOIN corrections as c ON u.id=c.user_id
          JOIN projects AS p ON c.project_id=p.id
-         GROUP BY u.id);
+         GROUP BY u.id)
      AS weighted_av SET u.average_score = weighted_av.wa
-     WHERE u.id = weighted_av.id
+     WHERE u.id = weighted_av.id;
 END
 //
 
--- 0x00-MySQL_Advanced/101-init.sql
+++ 0x00-MySQL_Advanced/101-init.sql
@@ -45,3 +45,4 @@ INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project
 
 INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_c, 91);
 INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_py, 73);
+
