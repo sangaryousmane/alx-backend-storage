fa0280a53a5a12f70d7076111557fb7e8764ca58
sangaryousmane <sangaryousmane@gmail.com>
2024-03-24 18:49:02 +0000

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -10,7 +10,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users 
 	SET average_score = (SELECT AVG(score) FROM corrections as c WHERE c.user_id=user_id)
-       	WHERE id=user_id
+       	WHERE id=user_id;
 END;
 $$
 DELIMITER ;
