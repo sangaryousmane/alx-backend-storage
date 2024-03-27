3c0e47e00761db1aa383a593eadaf46e80fd4714
sangaryousmane <sangaryousmane@gmail.com>
2024-03-26 19:07:02 -0700

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" function that returns all students sorted by average score:
+"""
+
+
+def top_students(mongo_collection):
+    """ function that returns all students sorted by average score:
+    """
+    students = mongo_collection.aggregate([{
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}}
+            },
+            {"$sort": {"averageScore": -1}}
+            ])
+    return students
