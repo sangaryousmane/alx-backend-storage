70e8928761f6a0bc3e923c12d3a33cffdca7667a
sangaryousmane <sangaryousmane@gmail.com>
2024-03-26 18:57:00 -0700

    Update many columns base on the name

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,5 +6,4 @@
 def updat_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name"""
     updated = mongo_collection.update_many(
-            {"name": name}, {$set: {"topics": topics}})
-    return updated.modified_count
+            {"name": name}, {"$set": {"topics": topics}})
